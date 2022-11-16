0bde7895f5ac99fb65f1128ee53e7ee080802ea9
neIIIexog777 <neIIIexog@gmail.com>
2022-11-16 14:12:51 +0300

    first task

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    //упражнение №1
+    public static void main(String[] args) {
+
+
+        char[] name = {80,97,118,101,108};
+        for(char i : name){
+            System.out.print(i);
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
